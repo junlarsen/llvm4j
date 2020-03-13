498e29e553b139ab1f32d6d0bd4762b516fd5979
supergrecko <me@supergrecko.com>
2020-03-13 16:29:14 +0100

    [api] Make object methods @JvmStatic

--- src/main/kotlin/dev/supergrecko/kllvm/core/type/IntegerType.kt
+++ src/main/kotlin/dev/supergrecko/kllvm/core/type/IntegerType.kt
@@ -13,7 +13,10 @@ object IntegerType {
      *
      * - [LLVMInt1TypeInContext](https://llvm.org/doxygen/group__LLVMCCoreTypeInt.html#ga390b4c486c780eed40002b07933d13df)
      */
+    @JvmStatic
     public fun i1Type(): LLVMTypeRef = i1Type(LLVM.LLVMGetGlobalContext())
+
+    @JvmStatic
     public fun i1Type(context: LLVMContextRef): LLVMTypeRef {
         require(!context.isNull)
 
@@ -25,7 +28,10 @@ object IntegerType {
      *
      * - [LLVMInt8TypeInContext](https://llvm.org/doxygen/group__LLVMCCoreTypeInt.html#ga7afaa9a2cb5dd3c5c06d65298ed195d4)
      */
+    @JvmStatic
     public fun i8Type(): LLVMTypeRef = i8Type(LLVM.LLVMGetGlobalContext())
+
+    @JvmStatic
     public fun i8Type(context: LLVMContextRef): LLVMTypeRef {
         require(!context.isNull)
 
@@ -37,7 +43,10 @@ object IntegerType {
      *
      * - [LLVMInt16TypeInContext](https://llvm.org/doxygen/group__LLVMCCoreTypeInt.html#ga23a21172a069470b344a61672b299968)
      */
+    @JvmStatic
     public fun i16Type(): LLVMTypeRef = i16Type(LLVM.LLVMGetGlobalContext())
+
+    @JvmStatic
     public fun i16Type(context: LLVMContextRef): LLVMTypeRef {
         require(!context.isNull)
 
@@ -49,7 +58,10 @@ object IntegerType {
      *
      * - [LLVMInt32TypeInContext](https://llvm.org/doxygen/group__LLVMCCoreTypeInt.html#ga5e69a2cc779db154a0b805ed6ad3c724)
      */
+    @JvmStatic
     public fun i32Type(): LLVMTypeRef = i32Type(LLVM.LLVMGetGlobalContext())
+
+    @JvmStatic
     public fun i32Type(context: LLVMContextRef): LLVMTypeRef {
         require(!context.isNull)
 
@@ -61,7 +73,10 @@ object IntegerType {
      *
      * - [LLVMInt64TypeInContext](https://llvm.org/doxygen/group__LLVMCCoreTypeInt.html#ga23a21172a069470b344a61672b299968)
      */
+    @JvmStatic
     public fun i64Type(): LLVMTypeRef = i64Type(LLVM.LLVMGetGlobalContext())
+
+    @JvmStatic
     public fun i64Type(context: LLVMContextRef): LLVMTypeRef {
         require(!context.isNull)
 
@@ -73,7 +88,10 @@ object IntegerType {
      *
      * - [LLVMInt128TypeInContext](https://llvm.org/doxygen/group__LLVMCCoreTypeInt.html#ga5f3cfd960e39ae448213d45db5da229a)
      */
+    @JvmStatic
     public fun i128Type(): LLVMTypeRef = i128Type(LLVM.LLVMGetGlobalContext())
+
+    @JvmStatic
     public fun i128Type(context: LLVMContextRef): LLVMTypeRef {
         require(!context.isNull)
 
@@ -85,7 +103,10 @@ object IntegerType {
      *
      * - [LLVMIntIntTypeInContext](https://llvm.org/doxygen/group__LLVMCCoreTypeInt.html#ga2e5db8cbc30daa156083f2c42989138d)
      */
+    @JvmStatic
     public fun iType(size: Int): LLVMTypeRef = iType(size, LLVM.LLVMGetGlobalContext())
+
+    @JvmStatic
     public fun iType(size: Int, context: LLVMContextRef): LLVMTypeRef {
         require(!context.isNull)
         require(size in 1..8388606) { "LLVM only supports integers of 2^23-1 bits size" }
