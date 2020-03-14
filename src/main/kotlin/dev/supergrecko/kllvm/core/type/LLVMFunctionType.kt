d6b1a76bb5514aeb27138f400a09f7696275fb95
supergrecko <me@supergrecko.com>
2020-03-14 15:20:06 +0100

    [api] Implement LLVM Core APIs for LLVM Function Types

--- src/main/kotlin/dev/supergrecko/kllvm/core/type/LLVMFunctionType.kt
+++ src/main/kotlin/dev/supergrecko/kllvm/core/type/LLVMFunctionType.kt
@@ -1,12 +1,47 @@
 package dev.supergrecko.kllvm.core.type
 
 import dev.supergrecko.kllvm.contracts.CompositeTypeFactory
+import dev.supergrecko.kllvm.utils.toBoolean
 import dev.supergrecko.kllvm.utils.toInt
 import org.bytedeco.javacpp.PointerPointer
 import org.bytedeco.llvm.LLVM.LLVMTypeRef
 import org.bytedeco.llvm.global.LLVM
 
-public class LLVMFunctionType internal constructor(llvmType: LLVMTypeRef) : LLVMType(llvmType) {
+/**
+ * Wrapper around LLVM Function Types
+ *
+ * @property llvmType Internal [LLVMTypeRef] reference
+ * @property returnType [LLVMType] for function return type
+ * @property paramTypes [LLVMType] list for function parameters
+ *
+ * TODO: Determine whether passing [returnType] and [paramTypes] is okay (does LLVM use this elsewhere?)
+ */
+public class LLVMFunctionType internal constructor(
+        llvmType: LLVMTypeRef,
+        internal val returnType: LLVMType,
+        internal val paramTypes: List<LLVMType>
+) : LLVMType(llvmType) {
+    /**
+     * Is this function type variadic?
+     *
+     * TODO: Determine whether passing this from [LLVMFunctionType.type] is more optimal
+     */
+    public fun isVariadic(): Boolean {
+        return LLVM.LLVMIsFunctionVarArg(llvmType).toBoolean()
+    }
+
+    /**
+     * Get how many parameters this function type expects
+     *
+     * TODO: Determine whether passing this from [LLVMFunctionType.type] is more optimal
+     */
+    public fun getParameterCount(): Int {
+        return LLVM.LLVMCountParamTypes(llvmType)
+    }
+
+    public fun getReturnType(): LLVMType = returnType
+    public fun getParameters(): List<LLVMType> = paramTypes
+
     public companion object : CompositeTypeFactory<LLVMFunctionType> {
         /**
          * Create a function type
@@ -21,7 +56,7 @@ public class LLVMFunctionType internal constructor(llvmType: LLVMTypeRef) : LLVM
 
             val type = LLVM.LLVMFunctionType(returnType.llvmType, ptr, paramTypes.size, isVariadic.toInt())
 
-            return LLVMFunctionType(type)
+            return LLVMFunctionType(type, returnType, paramTypes)
         }
     }
 }
