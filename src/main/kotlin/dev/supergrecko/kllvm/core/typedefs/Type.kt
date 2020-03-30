2f06e7b2311f3f077aa77b9fa84cfe8130800204
supergrecko <me@supergrecko.com>
2020-03-30 16:56:40 +0200

    [api] Clean up Type and Value

--- src/main/kotlin/dev/supergrecko/kllvm/core/typedefs/Type.kt
+++ src/main/kotlin/dev/supergrecko/kllvm/core/typedefs/Type.kt
@@ -12,14 +12,23 @@ public open class Type internal constructor(
         internal val llvmType: LLVMTypeRef
 ) {
     //region Core::Types
+    /**
+     * @see [LLVM.LLVMGetTypeKind]
+     */
     public fun getTypeKind(): TypeKind {
         return getTypeKind(llvmType)
     }
 
+    /**
+     * @see [LLVM.LLVMTypeIsSized]
+     */
     public fun isSized(): Boolean {
         return LLVM.LLVMTypeIsSized(llvmType).toBoolean()
     }
 
+    /**
+     * @see [LLVM.LLVMGetTypeContext]
+     */
     public fun getContext(): Context {
         val ctx = LLVM.LLVMGetTypeContext(llvmType)
 
@@ -30,6 +39,8 @@ public open class Type internal constructor(
      * Moves the string representation into a Message
      *
      * This message must be disposed via [Message.dispose] otherwise memory will be leaked.
+     *
+     * @see [LLVM.LLVMPrintTypeToString]
      */
     public fun getStringRepresentation(): Message {
         val ptr = LLVM.LLVMPrintTypeToString(llvmType)
@@ -39,6 +50,9 @@ public open class Type internal constructor(
     //endregion Core::Types
 
     //region Core::Values::Constants
+    /**
+     * @see [LLVM.LLVMConstNull]
+     */
     public fun getConstantNull(): Value {
         // Opaque structures cannot have a null type
         if (this is StructType) {
@@ -48,23 +62,28 @@ public open class Type internal constructor(
         return Value(LLVM.LLVMConstNull(llvmType))
     }
 
+    /**
+     * @see [LLVM.LLVMGetUndef]
+     */
     public fun getConstantUndef(): Value {
         return Value(LLVM.LLVMGetUndef(llvmType))
     }
 
+    /**
+     * @see [LLVM.LLVMConstPointerNull]
+     */
     public fun getConstantNullPointer(): Value {
         return Value(LLVM.LLVMConstPointerNull(llvmType))
     }
     //endregion Core::Values::Constants
 
-    // TODO: refactor with factories
+    //region Typecasting
     public fun toPointerType(addressSpace: Int = 0): PointerType = PointerType.new(this, addressSpace)
 
     public fun toArrayType(size: Int): ArrayType = ArrayType.new(this, size)
 
     public fun toVectorType(size: Int): VectorType = VectorType.new(this, size)
 
-    //region Typecasting
     public inline fun <reified T : Type> cast(): T {
         val ctor: Constructor<T> = T::class.java.getDeclaredConstructor(LLVMTypeRef::class.java)
 
@@ -72,8 +91,6 @@ public open class Type internal constructor(
                 // Should theoretically be unreachable
                 ?: throw TypeCastException("Failed to cast LLVMType to T")
     }
-
-    public fun getUnderlyingReference(): LLVMTypeRef = llvmType
 
     public fun asArrayType(): ArrayType = ArrayType(llvmType)
     public fun asFloatType(): FloatType = FloatType(llvmType)
@@ -85,8 +102,15 @@ public open class Type internal constructor(
     public fun asVoidType(): VoidType = VoidType(llvmType)
     //endregion Typecasting
 
+    public fun getUnderlyingReference(): LLVMTypeRef = llvmType
+
     companion object {
         @JvmStatic
+        /**
+         * @see [LLVM.LLVMGetTypeKind]
+         *
+         * @throws IllegalArgumentException If the type kind enum returns an invalid value
+         */
         public fun getTypeKind(type: LLVMTypeRef): TypeKind {
             val kind = LLVM.LLVMGetTypeKind(type)
 
