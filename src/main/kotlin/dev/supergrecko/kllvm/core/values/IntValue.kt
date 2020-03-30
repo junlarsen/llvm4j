bb13bfce1844efcabed356b1154bfa384a3dfd8e
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-03-30 08:17:46 +0530

    [api] Implement more APIs

--- src/main/kotlin/dev/supergrecko/kllvm/core/values/IntValue.kt
+++ src/main/kotlin/dev/supergrecko/kllvm/core/values/IntValue.kt
@@ -1,10 +1,14 @@
 package dev.supergrecko.kllvm.core.values
 
+import dev.supergrecko.kllvm.core.typedefs.Type
 import dev.supergrecko.kllvm.core.typedefs.Value
+import dev.supergrecko.kllvm.core.types.IntType
+import dev.supergrecko.kllvm.utils.toInt
 import org.bytedeco.llvm.LLVM.LLVMValueRef
 import org.bytedeco.llvm.global.LLVM
 
 public class IntValue(llvmValue: LLVMValueRef) : Value(llvmValue) {
+
     //region Core::Values::Constants::ScalarConstants
     public fun getIntZeroExtended(): Long {
         return LLVM.LLVMConstIntGetZExtValue(llvmValue)
@@ -14,4 +18,18 @@ public class IntValue(llvmValue: LLVMValueRef) : Value(llvmValue) {
         return LLVM.LLVMConstIntGetSExtValue(llvmValue)
     }
     //endregion Core::Values::Constants::ScalarConstants
+
+    companion object {
+        //region Core::Values::Constants::ScalarConstants
+        public fun new(type: Type, value: Long, signExtend: Boolean): IntValue {
+            return IntValue(
+                LLVM.LLVMConstInt(
+                    type.getUnderlyingReference(),
+                    value,
+                    signExtend.toInt()
+                )
+            )
+        }
+        //endregion Core::Values::Constants::ScalarConstants
+    }
 }
