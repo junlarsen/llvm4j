51316fab5619cd42a0130d06261115b5fa989c25
supergrecko <me@supergrecko.com>
2020-04-02 15:21:38 +0200

    [unittest] Add test for MemoryBuffer.getModule

--- src/test/kotlin/dev/supergrecko/kllvm/core/modules/ModuleTest.kt
+++ src/test/kotlin/dev/supergrecko/kllvm/core/modules/ModuleTest.kt
@@ -18,6 +18,8 @@ class ModuleTest {
         mod.setModuleIdentifier("test")
 
         assertEquals("test", mod.getModuleIdentifier())
+
+        mod.dispose()
     }
 
     @Test
@@ -28,6 +30,8 @@ class ModuleTest {
         val clone = mod.clone()
 
         assertEquals(mod.getModuleIdentifier(), clone.getModuleIdentifier())
+
+        mod.dispose()
     }
 
     @Test
@@ -39,12 +43,16 @@ class ModuleTest {
         mod.setSourceFileName("test2.ll")
 
         assertEquals("test2.ll", mod.getSourceFileName())
+
+        mod.dispose()
     }
 
     @Test
     fun `test getFunction returns null when no function added`() {
         val module = Module("test.ll")
         assertNull(module.getFunction("test"))
+
+        module.dispose()
     }
 
     @Test
@@ -52,6 +60,8 @@ class ModuleTest {
         val module = Module("test.ll")
         module.addFunction("test", FunctionType(VoidType(), listOf(), false))
         assertNotNull(module.getFunction("test"))
+
+        module.dispose()
     }
 
     @Test
@@ -77,6 +87,21 @@ class ModuleTest {
         val buf = module.toMemoryBuffer()
 
         val mod = buf.parse(context)
+
+        assertEquals("test.ll", mod.getSourceFileName())
+
+        module.dispose()
+        context.dispose()
+    }
+
+    @Test
+    fun `getting module from buffer works`() {
+        val context = Context()
+        val module = Module("test.ll", context)
+
+        val buf = module.toMemoryBuffer()
+
+        val mod = buf.getModule(context)
 
         assertEquals("test.ll", mod.getSourceFileName())
     }
