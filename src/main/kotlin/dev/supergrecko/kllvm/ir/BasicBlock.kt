2f13a7d1d2ad12063e915ca6b93512d699f159f0
Dhruv Rajvanshi <dhruvrajvanshi@outlook.com>
2020-05-09 14:25:21 +0530

    [api] Make BasicBlock.ref public

--- src/main/kotlin/dev/supergrecko/kllvm/ir/BasicBlock.kt
+++ src/main/kotlin/dev/supergrecko/kllvm/ir/BasicBlock.kt
@@ -3,7 +3,7 @@ package dev.supergrecko.kllvm.ir
 import org.bytedeco.llvm.LLVM.LLVMBasicBlockRef
 
 public class BasicBlock internal constructor() {
-    internal lateinit var ref: LLVMBasicBlockRef
+    public lateinit var ref: LLVMBasicBlockRef
 
     /**
      * Construct a new Type from an LLVM pointer reference
