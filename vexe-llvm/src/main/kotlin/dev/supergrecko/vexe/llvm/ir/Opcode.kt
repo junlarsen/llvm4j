2799633fc1bc14fd82562b8bfcb45211dfa4688f
supergrecko <me@supergrecko.com>
2020-06-11 15:20:12 +0200

    [llvm] Add remaining Instruction Opcodes

--- vexe-llvm/src/main/kotlin/dev/supergrecko/vexe/llvm/ir/Opcode.kt
+++ vexe-llvm/src/main/kotlin/dev/supergrecko/vexe/llvm/ir/Opcode.kt
@@ -14,8 +14,10 @@ enum class Opcode(public override val value: Int) : OrderedEnum<Int> {
     Switch(LLVM.LLVMSwitch),
     IndirectBr(LLVM.LLVMIndirectBr),
     Invoke(LLVM.LLVMInvoke),
+
     Unreachable(LLVM.LLVMUnreachable),
     CallBr(LLVM.LLVMCallBr),
+
     FNeg(LLVM.LLVMFNeg),
     Add(LLVM.LLVMAdd),
     FAdd(LLVM.LLVMFAdd),
@@ -29,16 +31,19 @@ enum class Opcode(public override val value: Int) : OrderedEnum<Int> {
     URem(LLVM.LLVMURem),
     SRem(LLVM.LLVMSRem),
     FRem(LLVM.LLVMFRem),
+
     Shl(LLVM.LLVMShl),
     LShr(LLVM.LLVMLShr),
     AShr(LLVM.LLVMAShr),
     And(LLVM.LLVMAnd),
     Or(LLVM.LLVMOr),
     Xor(LLVM.LLVMXor),
+
     Alloca(LLVM.LLVMAlloca),
     Load(LLVM.LLVMLoad),
     Store(LLVM.LLVMStore),
     GetElementPtr(LLVM.LLVMGetElementPtr),
+
     Trunc(LLVM.LLVMTrunc),
     ZExt(LLVM.LLVMZExt),
     SExt(LLVM.LLVMSExt),
@@ -51,5 +56,31 @@ enum class Opcode(public override val value: Int) : OrderedEnum<Int> {
     PtrToInt(LLVM.LLVMPtrToInt),
     IntToPtr(LLVM.LLVMIntToPtr),
     BitCast(LLVM.LLVMBitCast),
+    AddrSpaceCast(LLVM.LLVMAddrSpaceCast),
+
     ICmp(LLVM.LLVMICmp),
+    FCmp(LLVM.LLVMFCmp),
+    PHI(LLVM.LLVMPHI),
+    Call(LLVM.LLVMCall),
+    Select(LLVM.LLVMSelect),
+    UserOp1(LLVM.LLVMUserOp1),
+    UserOp2(LLVM.LLVMUserOp2),
+    VAArg(LLVM.LLVMVAArg),
+    ExtractElement(LLVM.LLVMExtractElement),
+    InsertElement(LLVM.LLVMInsertElement),
+    ShuffleVector(LLVM.LLVMShuffleVector),
+    ExtractValue(LLVM.LLVMExtractValue),
+    InsertValue(LLVM.LLVMInsertValue),
+
+    Fence(LLVM.LLVMFence),
+    AtomicCmpXChg(LLVM.LLVMAtomicCmpXchg),
+    AtomicRMW(LLVM.LLVMAtomicRMW),
+
+    Resume(LLVM.LLVMResume),
+    LandingPad(LLVM.LLVMLandingPad),
+    CleanupRet(LLVM.LLVMCleanupRet),
+    CatchRet(LLVM.LLVMCatchRet),
+    CatchPad(LLVM.LLVMCatchPad),
+    CleanupPad(LLVM.LLVMCleanupPad),
+    CatchSwitch(LLVM.LLVMCatchSwitch)
 }
