7afdc964534cd0f118d942716b9e17f1e2ea321e
supergrecko <me@supergrecko.com>
2020-06-11 15:04:22 +0200

    [llvm] Implement Terminators

---
+++ vexe-llvm/src/main/kotlin/dev/supergrecko/vexe/llvm/ir/instructions/BranchInstruction.kt
@@ -0,0 +1,47 @@
+package dev.supergrecko.vexe.llvm.ir.instructions
+
+import dev.supergrecko.vexe.llvm.internal.util.fromLLVMBool
+import dev.supergrecko.vexe.llvm.ir.Instruction
+import dev.supergrecko.vexe.llvm.ir.Value
+import org.bytedeco.llvm.LLVM.LLVMValueRef
+import org.bytedeco.llvm.global.LLVM
+
+public class BranchInstruction internal constructor() :
+    Instruction() {
+    public constructor(llvmValue: LLVMValueRef) : this() {
+        ref = llvmValue
+    }
+
+    //region Core::Instructions::Terminators
+    /**
+     * Is this branch conditional?
+     *
+     * @see LLVM.LLVMIsConditional
+     */
+    public fun isConditional(): Boolean {
+        return LLVM.LLVMIsConditional(ref).fromLLVMBool()
+    }
+
+    /**
+     * Get the condition for the branch
+     *
+     * @see LLVM.LLVMGetCondition
+     */
+    public fun getCondition(): Value {
+        require(isConditional())
+
+        val value = LLVM.LLVMGetCondition(ref)
+
+        return Value(value)
+    }
+
+    /**
+     * Set the condition for the branch
+     *
+     * @see LLVM.LLVMSetCondition
+     */
+    public fun setCondition(value: Value) {
+        LLVM.LLVMSetCondition(ref, value.ref)
+    }
+    //endregion Core::Instructions::Terminators
+}
