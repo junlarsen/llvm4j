861a3fe79d373672de4e8442cb5f85178923df4d
supergrecko <me@supergrecko.com>
2020-06-15 15:52:15 +0200

    [unittest] Update tests with new DSL

--- vexe-llvm/src/test/kotlin/dev/supergrecko/vexe/llvm/unit/ir/InstructionTest.kt
+++ vexe-llvm/src/test/kotlin/dev/supergrecko/vexe/llvm/unit/ir/InstructionTest.kt
@@ -1,5 +1,7 @@
 package dev.supergrecko.vexe.llvm.unit.ir
 
-import dev.supergrecko.vexe.llvm.utils.TestSuite
+import dev.supergrecko.vexe.test.TestSuite
 
-internal class InstructionTest : TestSuite()
+internal class InstructionTest : TestSuite({
+
+})
