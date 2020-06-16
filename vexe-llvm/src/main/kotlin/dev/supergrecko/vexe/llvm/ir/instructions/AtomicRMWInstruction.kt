ea2101c538e5ad8efcb57ab1183aeab88580236f
supergrecko <me@supergrecko.com>
2020-06-16 17:42:27 +0200

    [llvm] Finish APIs for Atomic instructions

--- vexe-llvm/src/main/kotlin/dev/supergrecko/vexe/llvm/ir/instructions/AtomicRMWInstruction.kt
+++ vexe-llvm/src/main/kotlin/dev/supergrecko/vexe/llvm/ir/instructions/AtomicRMWInstruction.kt
@@ -1,11 +1,12 @@
 package dev.supergrecko.vexe.llvm.ir.instructions
 
 import dev.supergrecko.vexe.llvm.ir.Instruction
+import dev.supergrecko.vexe.llvm.ir.instructions.traits.Atomic
 import dev.supergrecko.vexe.llvm.ir.instructions.traits.MemoryAccessor
 import org.bytedeco.llvm.LLVM.LLVMValueRef
 
 public class AtomicRMWInstruction internal constructor() : Instruction(),
-    MemoryAccessor {
+    MemoryAccessor, Atomic {
     public constructor(llvmValue: LLVMValueRef) : this() {
         ref = llvmValue
     }
