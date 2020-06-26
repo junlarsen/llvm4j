dd1261ce9f0bba21a9c6ea63e804e2613bfb35c8
supergrecko <me@supergrecko.com>
2020-06-26 20:30:38 +0200

    [tests] Create tests for callbacks

---
+++ src/test/kotlin/dev/supergrecko/vexe/llvm/unit/internal/CallbackTest.kt
@@ -0,0 +1,18 @@
+package dev.supergrecko.vexe.llvm.unit.internal
+
+import dev.supergrecko.vexe.llvm.ir.Context
+import dev.supergrecko.vexe.llvm.utils.cleanup
+import dev.supergrecko.vexe.test.TestSuite
+
+internal class CallbackTest : TestSuite({
+    describe("Creating a callback") {
+        val ctx = Context().apply {
+            // Kotlin lambdas!
+            setDiagnosticHandler({ _, _ ->
+
+            })
+        }
+
+        cleanup(ctx)
+    }
+})
