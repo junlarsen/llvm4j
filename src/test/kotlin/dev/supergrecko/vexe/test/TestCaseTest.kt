eeb79f6d52e616b0e6215a84a130f18cdbfd9d4f
supergrecko <me@supergrecko.com>
2020-06-27 16:21:38 +0200

    [testing] Set up Spek memoized values, remove old testcases

--- src/test/kotlin/dev/supergrecko/vexe/test/TestCaseTest.kt
+++
@@ -1,9 +0,0 @@
-package dev.supergrecko.vexe.test
-
-import kotlin.test.assertEquals
-
-internal class TestCaseTest : TestSuite({
-    describe("Hello World") {
-        assertEquals(1, 1)
-    }
-})
