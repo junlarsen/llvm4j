c683edf5df45db3fb5a49a0547ff5063dc2adc9f
supergrecko <me@supergrecko.com>
2020-06-28 21:18:58 +0200

    [tests] Update tests for Modules (#128)
    * [tests] Update tests for Modules

--- src/test/kotlin/dev/supergrecko/vexe/llvm/unit/ir/ModuleTest.kt
+++ src/test/kotlin/dev/supergrecko/vexe/llvm/unit/ir/ModuleTest.kt
@@ -1,206 +1,186 @@
 package dev.supergrecko.vexe.llvm.unit.ir
 
+import dev.supergrecko.vexe.llvm.TestUtils
 import dev.supergrecko.vexe.llvm.ir.Context
+import dev.supergrecko.vexe.llvm.ir.Metadata
 import dev.supergrecko.vexe.llvm.ir.Module
-import dev.supergrecko.vexe.llvm.ir.types.FunctionType
+import dev.supergrecko.vexe.llvm.ir.ModuleFlagBehavior
 import dev.supergrecko.vexe.llvm.ir.types.IntType
 import dev.supergrecko.vexe.llvm.ir.types.StructType
 import dev.supergrecko.vexe.llvm.ir.types.VoidType
+import dev.supergrecko.vexe.llvm.ir.values.constants.ConstantInt
+import dev.supergrecko.vexe.llvm.setup
 import dev.supergrecko.vexe.llvm.support.VerifierFailureAction
-import dev.supergrecko.vexe.llvm.utils.cleanup
-import dev.supergrecko.vexe.test.TestSuite
+import org.spekframework.spek2.Spek
+import java.nio.file.Files
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
 import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
-internal class ModuleTest : TestSuite({
-    describe("Modules can have identifiers") {
-        val mod = Module("utils.ll").apply {
-            setModuleIdentifier("utils")
+internal object ModuleTest : Spek({
+    setup()
+
+    val module: Module by memoized()
+    val utils: TestUtils by memoized()
+
+    group("module source file names") {
+        test("assigning a module identifier") {
+            module.setSourceFileName("test.tmp")
+
+            assertEquals("test.tmp", module.getSourceFileName())
         }
 
-        assertEquals("utils", mod.getModuleIdentifier())
+        test("names are preserved after module cloning") {
+            module.setSourceFileName("test.tmp")
 
-        cleanup(mod)
+            val subject = module.clone()
+
+            assertEquals("test.tmp", subject.getSourceFileName())
+        }
     }
 
-    describe("Cloning a module clones the identifier") {
-        val mod = Module("utils.ll").apply {
-            setModuleIdentifier("utils")
+    group("module identifier names") {
+        test("assigning a module identifier") {
+            module.setModuleIdentifier("id")
+
+            assertEquals("id", module.getModuleIdentifier())
         }
 
-        val clone = mod.clone()
+        test("ids are preserved after module cloning") {
+            module.setModuleIdentifier("id")
 
-        assertEquals(mod.getModuleIdentifier(), clone.getModuleIdentifier())
+            val subject = module.clone()
 
-        cleanup(mod, clone)
+            assertEquals("id", subject.getModuleIdentifier())
+        }
     }
 
-    describe("Modifying the module's source file name") {
-        val mod = Module("utils.ll").apply {
-            assertEquals("utils.ll", getSourceFileName())
+    group("module data layouts") {
+        test("assigning a data layout") {
+            module.setDataLayout("p:64:64:64")
 
-            setSourceFileName("test2.ll")
-
-            assertEquals("test2.ll", getSourceFileName())
+            assertEquals("p:64:64:64", module.getDataLayout())
         }
 
-        cleanup(mod)
+        test("layouts are preserved after module cloning") {
+            module.setDataLayout("p:64:64:64")
+
+            val subject = module.clone()
+
+            assertEquals("p:64:64:64", subject.getDataLayout())
+        }
     }
 
-    describe("Fetching a function which does not exist returns null") {
-        val module = Module("utils.ll")
+    group("module target triples") {
+        test("assigning a target triple") {
+            module.setTarget("x86_64-apple-macosx10.7.0")
 
-        assertNull(module.getFunction("utils"))
-
-        cleanup(module)
-    }
-
-    describe("Fetching an existing function returns said function") {
-        val module = Module("utils.ll").apply {
-            addFunction(
-                "utils",
-                FunctionType(
-                    VoidType(),
-                    listOf(),
-                    false
-                )
-            )
-
-            assertNotNull(getFunction("utils"))
+            assertEquals("x86_64-apple-macosx10.7.0", module.getTarget())
         }
 
-        cleanup(module)
+        test("targets are preserved after module cloning") {
+            module.setTarget("x86_64-apple-macosx10.7.0")
+
+            val subject = module.clone()
+
+            assertEquals("x86_64-apple-macosx10.7.0", subject.getTarget())
+        }
     }
 
-    describe("Write the module byte-code to file") {
-        val file = getTemporaryFile("out.bc")
-        val module = Module("utils.ll").also {
-            it.writeBitCodeToFile(file)
+    group("module flag entries") {
+        test("setting a metadata flag and finding it") {
+            val md = Metadata("example")
+            module.addModuleFlag(ModuleFlagBehavior.Override, "example", md)
+
+            val subject = module.getModuleFlag("example")
+
+            assertNotNull(subject)
         }
 
-        assertTrue { file.length() > 0 }
+        test("retrieving all the module flags") {
+            val md = Metadata("example")
+            module.addModuleFlag(ModuleFlagBehavior.Override, "example", md)
 
-        cleanup(module)
-    }
+            val subject = module.getModuleFlags()
 
-    describe("Write the module byte-code to file path") {
-        val file = getTemporaryFile("out.bc")
-        val module = Module("utils.ll").also {
-            it.writeBitCodeToFile(file.absolutePath)
+            assertEquals(1, subject.size())
+            assertEquals(ModuleFlagBehavior.Override, subject.getBehavior(0))
         }
 
-        assertTrue { file.exists() }
+        test("retrieving out of bounds will fail") {
+            val subject = module.getModuleFlags()
 
-        cleanup(module)
+            assertEquals(0, subject.size())
+            assertFailsWith<IndexOutOfBoundsException> {
+                subject.getKey(1)
+            }
+        }
     }
 
-    describe("Writing to MemoryBuffer") {
-        val context = Context()
-        val module = Module("utils.ll", context)
+    group("dumping the ir representation of the module") {
+        test("printing to string") {
+            val str = module.toString()
 
-        val buf = module.toMemoryBuffer()
-
-        assertNotNull(buf.ref)
-
-        cleanup(module, context)
-    }
-
-    describe("Get module from MemoryBuffer") {
-        val context = Context()
-        val module = Module("utils.ll", context)
-        val buf = module.toMemoryBuffer()
-        val mod = buf.getModule(context)
-
-        assertEquals("utils.ll", mod.getSourceFileName())
-
-        cleanup(mod, module, context)
-    }
-
-    describe("Verification of a valid module") {
-        val context = Context()
-        val module = Module("utils.ll", context)
-        val res = module.verify(VerifierFailureAction.ReturnStatus)
-
-        assertEquals(true, res)
-
-        cleanup(module, context)
-    }
-
-    describe("Creation of function inside module") {
-        val fnTy = FunctionType(IntType(32), listOf(), false)
-        val module = Module("utils.ll")
-        val fn = module.addFunction("utils", fnTy)
-
-        assertEquals(0, fn.getParameterCount())
-
-        cleanup(module)
-    }
-
-    describe("Assigning a data layout") {
-        val module = Module("utils.ll").apply {
-            setDataLayout("e-m:e-i64:64-f80:128-n8:16:32:64-S128")
+            assertTrue { str.isNotEmpty() }
         }
 
-        assertEquals(
-            "e-m:e-i64:64-f80:128-n8:16:32:64-S128", module.getDataLayout()
-        )
+        test("printing to file") {
+            val file = utils.getTemporaryFile()
+            val message = module.toFile(file.absolutePath)
+            val content = Files.readAllLines(file.toPath())
+                .joinToString("")
 
-        cleanup(module)
-    }
-
-    describe("Assigning a target triple") {
-        val module = Module("utils.ll").apply {
-            setTarget("x86_64-pc-linux")
+            assertNull(message)
+            assertTrue { content.isNotEmpty() }
         }
-
-        assertEquals("x86_64-pc-linux", module.getTarget())
-
-        cleanup(module)
     }
 
-    describe("Retrieving LLVM IR") {
-        val module = Module("utils.ll")
-        val ir = module.toString()
-
-        assertTrue { ir.isNotEmpty() }
-
-        cleanup(module)
-    }
-
-    describe("Setting module inline assembly") {
-        val module = Module("utils.ll").apply {
+    test("using inline assembler instructions") {
+        module.apply {
             setInlineAssembly(".example")
             appendInlineAssembly("    push 0")
             appendInlineAssembly("    ret")
         }
-        val result = ".example\n    push 0\n    ret\n"
 
-        // LLVM Appends a new line after any inline-asm changes
-        assertEquals(result, module.getInlineAssembly())
+        val expected = ".example\n    push 0\n    ret\n"
 
-        cleanup(module)
+        assertEquals(expected, module.getInlineAssembly())
     }
 
-    describe("Retrieving Context from Module") {
-        val ctx = Context()
-        val module = Module("utils.ll", ctx)
+    test("retrieving the context of the module") {
+        // Our Spek memoized value uses the global Context
+        val ctx = module.getContext()
+        val global = Context.getGlobalContext()
 
-        assertEquals(ctx.ref, module.getContext().ref)
-
-        cleanup(module, ctx)
+        assertEquals(global.ref, ctx.ref)
     }
 
-    describe("Retrieving a type from a module") {
-        val ctx = Context()
-        val type = StructType("TestStruct", ctx)
-        val module = Module("utils.ll", ctx)
-        val found = module.getTypeByName("TestStruct")
+    test("finding types inside a module") {
+        // Both uses global Context
+        val type = StructType("EmptyType")
+        val subject = module.getTypeByName("EmptyType")
 
-        assertNotNull(found)
-        assertEquals(type.ref, found.ref)
+        assertEquals(type.ref, subject?.ref)
+    }
 
-        cleanup(module, ctx)
+    group("verification") {
+        test("verification of a valid module") {
+            val success = module.verify(VerifierFailureAction.PrintMessage)
+
+            assertTrue { success }
+        }
+
+        test("invalid module") {
+            module.addGlobal("Nothing", VoidType()).apply {
+                setInitializer(ConstantInt(IntType(32), 100))
+            }
+
+            val success = module.verify(VerifierFailureAction.ReturnStatus)
+
+            assertFalse { success }
+        }
     }
 })
