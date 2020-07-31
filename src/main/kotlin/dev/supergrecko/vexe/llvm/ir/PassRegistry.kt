3fd85f4dffb729ce4690bb00209c0669622471c2
supergrecko <me@supergrecko.com>
2020-07-31 04:42:30 +0200

    [refactor] Make PassRegistry a singleton object

--- src/main/kotlin/dev/supergrecko/vexe/llvm/ir/PassRegistry.kt
+++ src/main/kotlin/dev/supergrecko/vexe/llvm/ir/PassRegistry.kt
@@ -4,16 +4,8 @@ import dev.supergrecko.vexe.llvm.internal.contracts.ContainsReference
 import org.bytedeco.llvm.LLVM.LLVMPassRegistryRef
 import org.bytedeco.llvm.global.LLVM
 
-/**
- * The LLVM Pass Registry
- *
- * This registry will always return a reference to the same LLVM Pass Registry
- * as the registry is a singleton in LLVM.
- *
- * [See](https://llvm.org/doxygen/group__LLVMCCorePassRegistry.html)
- */
-public class PassRegistry public constructor() :
-    ContainsReference<LLVMPassRegistryRef> {
-    public override var ref: LLVMPassRegistryRef =
+public object PassRegistry : ContainsReference<LLVMPassRegistryRef> {
+    public override val ref: LLVMPassRegistryRef by lazy {
         LLVM.LLVMGetGlobalPassRegistry()
+    }
 }
