f3bd1ade8b6af64d3654ef22a0e37dae1415bf6d
supergrecko <me@supergrecko.com>
2020-08-17 19:24:17 +0200

    [refactor] Make the Metadata API more type safe and mirror the C++ API

--- src/main/kotlin/io/vexelabs/bitbuilder/llvm/ir/Metadata.kt
+++ src/main/kotlin/io/vexelabs/bitbuilder/llvm/ir/Metadata.kt
@@ -8,70 +8,79 @@ import org.bytedeco.llvm.LLVM.LLVMMetadataRef
 import org.bytedeco.llvm.LLVM.LLVMValueRef
 import org.bytedeco.llvm.global.LLVM
 
-public class Metadata internal constructor() :
-    ContainsReference<LLVMMetadataRef> {
-    public override lateinit var ref: LLVMMetadataRef
+public sealed class Metadata : ContainsReference<LLVMMetadataRef> {
+    public final override lateinit var ref: LLVMMetadataRef
         internal set
 
-    public constructor(llvmRef: LLVMMetadataRef) : this() {
+    /**
+     * Represent a [MetadataNode] which has been cast to a [Value]
+     */
+    public class MetadataAsValue(ref: LLVMValueRef) : Value(ref)
+
+    /**
+     * Represent a [Value] which has been cast to a [MetadataNode]
+     */
+    public class ValueAsMetadata(ref: LLVMMetadataRef) : MetadataNode(ref)
+
+    /**
+     * Cast this Metadata node to a value
+     *
+     * @see LLVM.LLVMMetadataAsValue
+     */
+    public fun toValue(
+        withContext: Context = Context.getGlobalContext()
+    ): MetadataAsValue {
+        val md = LLVM.LLVMMetadataAsValue(withContext.ref, ref)
+
+        return MetadataAsValue(md)
+    }
+
+    public companion object {
+        /**
+         * Cast a value to a [MetadataNode]
+         *
+         * @see LLVM.LLVMValueAsMetadata
+         */
+        public fun fromValue(value: Value): ValueAsMetadata {
+            val md = LLVM.LLVMValueAsMetadata(value.ref)
+
+            return ValueAsMetadata(md)
+        }
+
+        /**
+         * Cast this Metadata node to a value
+         *
+         * LLVM-C accepts a [LLVMValueRef] for this so the node is cast to a
+         * [Metadata.MetadataAsValue]. This requires a context. Set the context
+         * which this should use with [withContext]
+         *
+         * @see LLVM.LLVMMetadataAsValue
+         * @see LLVM.LLVMMetadataAsValue
+         */
+        public fun toValue(
+            metadata: Metadata,
+            withContext: Context = Context.getGlobalContext()
+        ): MetadataAsValue = metadata.toValue(withContext)
+    }
+}
+
+public class MetadataString : Metadata {
+    public constructor(llvmRef: LLVMMetadataRef) {
         ref = llvmRef
     }
 
     //region Core::Metadata
-    /**
-     * Create a string metadata node
-     *
-     * @see LLVM.LLVMMDStringInContext2
-     */
     public constructor(
-        string: String,
+        data: String,
         context: Context = Context.getGlobalContext()
-    ) : this() {
+    ) {
         ref = LLVM.LLVMMDStringInContext2(
             context.ref,
-            string,
-            string.length.toLong()
+            data,
+            data.length.toLong()
         )
     }
-
-    /**
-     * Create a metadata node
-     *
-     * @see LLVM.LLVMMDNodeInContext2
-     */
-    public constructor(
-        operands: List<Metadata>,
-        context: Context = Context.getGlobalContext()
-    ) : this() {
-        val ptr = PointerPointer(*operands.map { it.ref }.toTypedArray())
-        ref = LLVM.LLVMMDNodeInContext2(
-            context.ref,
-            ptr,
-            operands.size.toLong()
-        )
-    }
-
-    /**
-     * Create a metadata node from a value
-     *
-     * @see LLVM.LLVMValueAsMetadata
-     */
-    public constructor(value: Value) : this() {
-        ref = LLVM.LLVMValueAsMetadata(value.ref)
-    }
-
-    /**
-     * Get the current metadata as value
-     *
-     * @see LLVM.LLVMMetadataAsValue
-     */
-    public fun asValue(
-        context: Context = Context.getGlobalContext()
-    ): Value {
-        val v = LLVM.LLVMMetadataAsValue(context.ref, ref)
-
-        return Value(v)
-    }
+    //endregion Core::Metadata
 
     /**
      * Get the string from a MDString node
@@ -82,52 +91,68 @@ public class Metadata internal constructor() :
         context: Context = Context.getGlobalContext()
     ): String {
         val len = IntPointer(0)
-        val ptr = LLVM.LLVMGetMDString(asValue(context).ref, len)
+        val ptr = LLVM.LLVMGetMDString(toValue(context).ref, len)
 
         len.deallocate()
 
         return ptr.string
     }
+}
+
+public open class MetadataNode : Metadata {
+    public constructor(llvmRef: LLVMMetadataRef) {
+        ref = llvmRef
+    }
+
+    //region Core::Metadata
+    public constructor(
+        values: List<Metadata>,
+        context: Context = Context.getGlobalContext()
+    ) {
+        val ptr = PointerPointer(*values.map { it.ref }.toTypedArray())
+        ref = LLVM.LLVMMDNodeInContext2(
+            context.ref,
+            ptr,
+            values.size.toLong()
+        )
+    }
 
     /**
      * Get the amount of operands in a metadata node
      *
+     * LLVM-C accepts a [LLVMValueRef] for this so the node is cast to a
+     * [Metadata.MetadataAsValue]. This requires a context. Set the context
+     * which this should use with [withContext]
+     *
      * @see LLVM.LLVMGetMDNodeNumOperands
      */
     public fun getOperandCount(
-        context: Context = Context.getGlobalContext()
+        withContext: Context = Context.getGlobalContext()
     ): Int {
-        return LLVM.LLVMGetMDNodeNumOperands(asValue(context).ref)
+        return LLVM.LLVMGetMDNodeNumOperands(toValue(withContext).ref)
     }
 
     /**
      * Get the operands from a metadata node
      *
+     * LLVM-C accepts a [LLVMValueRef] for this so the node is cast to a
+     * [Metadata.MetadataAsValue]. This requires a context. Set the context
+     * which this should use with [withContext]
+     *
      * @see LLVM.LLVMGetMDNodeOperands
      */
     public fun getOperands(
-        context: Context = Context.getGlobalContext()
+        withContext: Context = Context.getGlobalContext()
     ): List<Value> {
         val count = getOperandCount().toLong()
         val ptr = PointerPointer<LLVMValueRef>(count)
 
         LLVM.LLVMGetMDNodeOperands(
-            asValue(context).ref,
+            toValue(withContext).ref,
             ptr
         )
 
         return ptr.map { Value(it) }
-    }
-
-    public companion object {
-        /**
-         * Create a metadata node from a value
-         *
-         * @see LLVM.LLVMValueAsMetadata
-         */
-        public fun fromValue(value: Value): Metadata {
-            return Metadata(value)
-        }
     }
     //endregion Core::Metadata
 }
