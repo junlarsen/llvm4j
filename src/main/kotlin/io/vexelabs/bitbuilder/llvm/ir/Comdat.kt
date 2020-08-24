4f35a257cc234833d20f25d9d4871aaa78d32fce
supergrecko <me@supergrecko.com>
2020-08-24 20:24:20 +0200

    [api] Create Global Value Comdat APIs

--- src/main/kotlin/io/vexelabs/bitbuilder/llvm/ir/Comdat.kt
+++ src/main/kotlin/io/vexelabs/bitbuilder/llvm/ir/Comdat.kt
@@ -1,7 +1,9 @@
 package io.vexelabs.bitbuilder.llvm.ir
 
 import io.vexelabs.bitbuilder.llvm.internal.contracts.ContainsReference
+import io.vexelabs.bitbuilder.llvm.internal.contracts.ForeignEnum
 import org.bytedeco.llvm.LLVM.LLVMComdatRef
+import org.bytedeco.llvm.global.LLVM
 
 public class Comdat internal constructor() : ContainsReference<LLVMComdatRef> {
     public override lateinit var ref: LLVMComdatRef
@@ -9,5 +11,40 @@ public class Comdat internal constructor() : ContainsReference<LLVMComdatRef> {
 
     public constructor(comdat: LLVMComdatRef) : this() {
         ref = comdat
+    }
+
+    /**
+     * Get the selection kind of this comdat
+     *
+     * @see LLVM.LLVMGetComdatSelectionKind
+     */
+    public fun getSelectionKind(): SelectionKind {
+        val kind = LLVM.LLVMGetComdatSelectionKind(ref)
+
+        return SelectionKind[kind]
+    }
+
+    /**
+     * Set the selection kind of this comdat
+     *
+     * @see LLVM.LLVMSetComdatSelectionKind
+     */
+    public fun setSelectionKind(kind: SelectionKind) {
+        LLVM.LLVMSetComdatSelectionKind(ref, kind.value)
+    }
+
+    public enum class SelectionKind(public override val value: Int) :
+        ForeignEnum<Int> {
+        Any(LLVM.LLVMAnyComdatSelectionKind),
+        ExactMatch(LLVM.LLVMExactMatchComdatSelectionKind),
+        Largest(LLVM.LLVMLargestComdatSelectionKind),
+        NoDuplicates(LLVM.LLVMNoDuplicatesComdatSelectionKind),
+        SameSize(LLVM.LLVMSameSizeComdatSelectionKind);
+
+        public companion object : ForeignEnum.CompanionBase<Int, SelectionKind> {
+            public override val map: Map<Int, SelectionKind> by lazy {
+                values().associateBy(SelectionKind::value)
+            }
+        }
     }
 }
