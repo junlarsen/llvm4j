1cb707c1da97423722ff2c646a1e60df486a6707
supergrecko <me@supergrecko.com>
2020-09-26 21:03:00 +0200

    [internal] add remaining cast paths

--- src/main/kotlin/io/vexelabs/bitbuilder/rtti/Casting.kt
+++ src/main/kotlin/io/vexelabs/bitbuilder/rtti/Casting.kt
@@ -1,14 +1,60 @@
 package io.vexelabs.bitbuilder.rtti
 
+import com.sun.jdi.FloatType
 import io.vexelabs.bitbuilder.llvm.internal.contracts.ContainsReference
 import io.vexelabs.bitbuilder.llvm.ir.Instruction
+import io.vexelabs.bitbuilder.llvm.ir.User
+import io.vexelabs.bitbuilder.llvm.ir.instructions.AllocaInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.AtomicCmpXchgInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.AtomicRMWInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.BrInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.CallBrInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.CallInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.CatchPadInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.CatchRetInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.CatchSwitchInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.CleanupPadInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.CleanupRetInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.FenceInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.IndirectBrInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.InvokeInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.LandingPadInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.LoadInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.PhiInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.ResumeInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.RetInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.SelectInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.StoreInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.SwitchInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.UnreachableInstruction
+import io.vexelabs.bitbuilder.llvm.ir.instructions.VAArgInstruction
+import io.vexelabs.bitbuilder.llvm.ir.types.FunctionType
+import io.vexelabs.bitbuilder.llvm.ir.types.IntType
+import io.vexelabs.bitbuilder.llvm.ir.types.LabelType
+import io.vexelabs.bitbuilder.llvm.ir.types.MetadataType
+import io.vexelabs.bitbuilder.llvm.ir.types.PointerType
+import io.vexelabs.bitbuilder.llvm.ir.types.StructType
+import io.vexelabs.bitbuilder.llvm.ir.types.TokenType
+import io.vexelabs.bitbuilder.llvm.ir.types.VectorType
+import io.vexelabs.bitbuilder.llvm.ir.types.VoidType
+import io.vexelabs.bitbuilder.llvm.ir.types.X86MMXType
+import io.vexelabs.bitbuilder.llvm.ir.values.ConstantValue
+import io.vexelabs.bitbuilder.llvm.ir.values.FunctionValue
+import io.vexelabs.bitbuilder.llvm.ir.values.GlobalAlias
+import io.vexelabs.bitbuilder.llvm.ir.values.GlobalValue
+import io.vexelabs.bitbuilder.llvm.ir.values.GlobalVariable
+import io.vexelabs.bitbuilder.llvm.ir.values.IndirectFunction
+import io.vexelabs.bitbuilder.llvm.ir.values.constants.ConstantArray
+import io.vexelabs.bitbuilder.llvm.ir.values.constants.ConstantFloat
 import io.vexelabs.bitbuilder.llvm.ir.values.constants.ConstantInt
 import io.vexelabs.bitbuilder.llvm.ir.values.constants.ConstantPointer
 import io.vexelabs.bitbuilder.llvm.ir.values.constants.ConstantStruct
 import io.vexelabs.bitbuilder.llvm.ir.values.constants.ConstantVector
 import org.bytedeco.javacpp.Pointer
+import org.bytedeco.llvm.LLVM.LLVMTypeRef
 import org.bytedeco.llvm.LLVM.LLVMValueRef
 import java.lang.reflect.Constructor
+import javax.lang.model.type.ArrayType
 
 /**
  * Helper function to retrieve the java.lang.Class<[T]> and the constructor of
@@ -18,7 +64,7 @@ import java.lang.reflect.Constructor
  *
  * This is only for internal usage to map out RTTI casting paths
  */
-private inline fun <reified T, reified P : Pointer> getConstructor():
+private inline fun <reified T, reified P : Pointer> path():
         Pair<Class<T>, Constructor<T>> {
     val clazz = T::class.java
     val ptr = P::class.java
@@ -30,15 +76,65 @@ private inline fun <reified T, reified P : Pointer> getConstructor():
  * A lazy map listing all the valid type casting paths, meaning that the
  * BitBuilder type can be constructed with the JavaCPP Pointer Type
  *
- * @see getConstructor
+ * @see path
  */
 public val constructorMap: Map<Class<*>, Constructor<*>> by lazy {
     mapOf(
-        getConstructor<ConstantInt, LLVMValueRef>(),
-        getConstructor<ConstantVector, LLVMValueRef>(),
-        getConstructor<ConstantStruct, LLVMValueRef>(),
-        getConstructor<ConstantPointer, LLVMValueRef>(),
-        getConstructor<Instruction, LLVMValueRef>(),
+        // Basic Values
+        path<FunctionValue, LLVMValueRef>(),
+        path<IndirectFunction, LLVMValueRef>(),
+        path<GlobalValue, LLVMValueRef>(),
+        path<GlobalVariable, LLVMValueRef>(),
+        path<GlobalAlias, LLVMValueRef>(),
+        // Constant Values
+        path<ConstantValue, LLVMValueRef>(),
+        path<ConstantInt, LLVMValueRef>(),
+        path<ConstantVector, LLVMValueRef>(),
+        path<ConstantStruct, LLVMValueRef>(),
+        path<ConstantPointer, LLVMValueRef>(),
+        path<ConstantFloat, LLVMValueRef>(),
+        path<ConstantArray, LLVMValueRef>(),
+        // Types
+        path<ArrayType, LLVMTypeRef>(),
+        path<FloatType, LLVMTypeRef>(),
+        path<FunctionType, LLVMTypeRef>(),
+        path<IntType, LLVMTypeRef>(),
+        path<LabelType, LLVMTypeRef>(),
+        path<MetadataType, LLVMTypeRef>(),
+        path<PointerType, LLVMTypeRef>(),
+        path<StructType, LLVMTypeRef>(),
+        path<TokenType, LLVMTypeRef>(),
+        path<VectorType, LLVMTypeRef>(),
+        path<VoidType, LLVMTypeRef>(),
+        path<X86MMXType, LLVMTypeRef>(),
+        // Users
+        path<User, LLVMValueRef>(),
+        // Instructions
+        path<Instruction, LLVMValueRef>(),
+        path<AllocaInstruction, LLVMValueRef>(),
+        path<AtomicCmpXchgInstruction, LLVMValueRef>(),
+        path<AtomicRMWInstruction, LLVMValueRef>(),
+        path<BrInstruction, LLVMValueRef>(),
+        path<CallBrInstruction, LLVMValueRef>(),
+        path<CallInstruction, LLVMValueRef>(),
+        path<CatchPadInstruction, LLVMValueRef>(),
+        path<CatchRetInstruction, LLVMValueRef>(),
+        path<CatchSwitchInstruction, LLVMValueRef>(),
+        path<CleanupPadInstruction, LLVMValueRef>(),
+        path<CleanupRetInstruction, LLVMValueRef>(),
+        path<FenceInstruction, LLVMValueRef>(),
+        path<IndirectBrInstruction, LLVMValueRef>(),
+        path<InvokeInstruction, LLVMValueRef>(),
+        path<LandingPadInstruction, LLVMValueRef>(),
+        path<LoadInstruction, LLVMValueRef>(),
+        path<PhiInstruction, LLVMValueRef>(),
+        path<ResumeInstruction, LLVMValueRef>(),
+        path<RetInstruction, LLVMValueRef>(),
+        path<SelectInstruction, LLVMValueRef>(),
+        path<StoreInstruction, LLVMValueRef>(),
+        path<SwitchInstruction, LLVMValueRef>(),
+        path<UnreachableInstruction, LLVMValueRef>(),
+        path<VAArgInstruction, LLVMValueRef>()
     )
 }
 
