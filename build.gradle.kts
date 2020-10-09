54491fda1b1dcfb8a1b5e4e3dd1ab5b843800f97
Reghill J4 <rj4@lenovo.com>
2020-10-09 22:55:23 +0530

    Update dependencies from kotlin 1.4.0 -rc to 1.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.github.johnrengelman.shadow") version "5.2.0"
     id("org.jetbrains.dokka") version "1.4.0-rc"
-    id("org.jetbrains.kotlin.jvm") version "1.4.0-rc"
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
     id("maven")
 }
 
@@ -15,13 +15,13 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.0-rc")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.10")
     implementation("org.bytedeco:llvm-platform:10.0.1-1.5.4")
 
-    testImplementation("org.jetbrains.kotlin:kotlin-test:1.4.0-rc")
+    testImplementation("org.jetbrains.kotlin:kotlin-test:1.4.10")
     testImplementation("org.spekframework.spek2:spek-dsl-jvm:2.0.11")
     testRuntimeOnly("org.spekframework.spek2:spek-runner-junit5:2.0.11")
-    testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.4.0-rc")
+    testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
 }
 
 kotlin {
