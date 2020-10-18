291d6b3d0110ac2c95289be93c0a3461d45d90c3
supergrecko <me@supergrecko.com>
2020-10-18 19:05:09 +0200

    [gradle] Set up gradle for publishing (#193)
    * [gradle] Set up gradle for publishing
    
    * [gradle] default to no value properties if value not found

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     id("com.github.johnrengelman.shadow") version "5.2.0"
     id("org.jlleitschuh.gradle.ktlint") version "9.4.1"
     id("org.jetbrains.dokka") version "1.4.0-rc"
     id("org.jetbrains.kotlin.jvm") version "1.4.10"
     id("maven")
+    id("maven-publish")
+    id("signing")
 }
 
 group = "io.vexelabs"
-version = "0.0.1-SNAPSHOT"
+version = "0.1.0-SNAPSHOT"
+
+kotlin.explicitApi()
+ktlint.debug.set(true)
+
+val isSnapshot = version.toString().endsWith("SNAPSHOT")
 
 repositories {
     mavenCentral()
@@ -25,29 +34,20 @@ dependencies {
     testRuntimeOnly("org.jetbrains.kotlin:kotlin-reflect:1.4.10")
 }
 
-kotlin {
-    explicitApi()
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    sourceCompatibility = "1.8"
-    targetCompatibility = "1.8"
-
-    kotlinOptions {
-        jvmTarget = "1.8"
+tasks {
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
     }
-}
 
-tasks.withType<Test>().configureEach {
-    useJUnitPlatform {
-        includeEngines("spek2")
+    withType<Test>().configureEach {
+        useJUnitPlatform {
+            includeEngines("spek2")
+        }
     }
-}
 
-tasks.dokkaHtml {
-    outputDirectory = "$buildDir/dokka"
-    dokkaSourceSets {
-        configureEach {
+    dokkaHtml {
+        outputDirectory = "$buildDir/javadoc"
+        dokkaSourceSets.configureEach {
             skipDeprecated = false
             includeNonPublic = false
             reportUndocumented = true
@@ -59,6 +59,71 @@ tasks.dokkaHtml {
     }
 }
 
-ktlint {
-    debug.set(true)
+val sourcesJar by tasks.creating(Jar::class) {
+    archiveClassifier.set("sources")
+    from(sourceSets["main"].allSource)
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+    group = JavaBasePlugin.DOCUMENTATION_GROUP
+    archiveClassifier.set("javadoc")
+    from(tasks.dokkaHtml)
+    dependsOn(tasks.dokkaHtml)
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("vexelabs") {
+            from(components["kotlin"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
+
+            repositories {
+                maven {
+                    url = if (isSnapshot) {
+                        uri("https://repo.vexelabs.io/snapshots/")
+                    } else {
+                        uri("https://repo.vexelabs.io/")
+                    }
+
+                    credentials {
+                        username = project.properties.getOrDefault("publishRepository.username", "default") as? String
+                        password = project.properties.getOrDefault("publishRepository.password", "default") as? String
+                    }
+                }
+            }
+
+            pom {
+                name.set("BitBuilder")
+                description.set("Kotlin interface to the LLVM APIs")
+                url.set("https://vexelabs.io/projects/bitbuilder")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("supergrecko")
+                        name.set("Mats Larsen")
+                        email.set("me@supergrecko.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:ssh://github.com/vexelabs/bitbuilder.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:vexelabs/bitbuilder.git")
+                    url.set("https://github.com/vexelabs/bitbuilder")
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications["vexelabs"])
 }
