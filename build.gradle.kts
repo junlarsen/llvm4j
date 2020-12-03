d010e102e709cabe1e20cacef721887323c8ab6f
supergrecko <me@supergrecko.com>
2020-12-03 12:18:17 +0100

    [fix] fix gradle publish task

--- build.gradle.kts
+++ build.gradle.kts
@@ -87,67 +87,71 @@ val javadocJar by tasks.creating(Jar::class) {
     dependsOn(tasks.dokkaHtml)
 }
 
-publishing.publications.create<MavenPublication>("sonatype") {
-    from(components["kotlin"])
-    artifact(sourcesJar)
-    artifact(javadocJar)
+publishing {
+    publications {
+        create<MavenPublication>("sonatype") {
+            from(components["kotlin"])
+            artifact(sourcesJar)
+            artifact(javadocJar)
 
-    repositories {
-        maven {
-            // Deploy all snapshots to OSSRH
-            // Releases on CI are published to vexelabs for testing
-            // Releases from user is published to Sonatype Staging
-            url = when {
-                isSnapshot -> uri("https://oss.sonatype.org/content/repositories/snapshots/")
-                else -> when {
-                    isCI -> uri("https://repo.vexelabs.io/releases/")
-                    else -> uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+            repositories {
+                maven {
+                    // Deploy all snapshots to OSSRH
+                    // Releases on CI are published to vexelabs for testing
+                    // Releases from user is published to Sonatype Staging
+                    url = when {
+                        isSnapshot -> uri("https://oss.sonatype.org/content/repositories/snapshots/")
+                        else -> when {
+                            isCI -> uri("https://repo.vexelabs.io/releases/")
+                            else -> uri("https://oss.sonatype.org/service/local/staging/deploy/maven2/")
+                        }
+                    }
+
+                    credentials {
+                        username = System.getenv("PUBLISH_USERNAME")
+                        password = System.getenv("PUBLISH_PASSWORD")
+                    }
                 }
             }
 
-            credentials {
-                username = System.getenv("PUBLISH_USERNAME")
-                password = System.getenv("PUBLISH_PASSWORD")
+            pom {
+                name.set("BitBuilder")
+                description.set("Kotlin interface to the LLVM APIs")
+                url.set("https://vexelabs.io/projects/bitbuilder")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("supergrecko")
+                        name.set("Mats Larsen")
+                        email.set("me@supergrecko.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:ssh://github.com/vexelabs/bitbuilder.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:vexelabs/bitbuilder.git")
+                    url.set("https://github.com/vexelabs/bitbuilder")
+                }
             }
         }
-    }
 
-    pom {
-        name.set("BitBuilder")
-        description.set("Kotlin interface to the LLVM APIs")
-        url.set("https://vexelabs.io/projects/bitbuilder")
+        signing {
+            if (isCI) {
+                val signingKey: String? by project
+                val signingPassword: String? by project
 
-        licenses {
-            license {
-                name.set("The Apache License, Version 2.0")
-                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                useInMemoryPgpKeys(signingKey, signingPassword)
+            } else {
+                useGpgCmd()
             }
-        }
-
-        developers {
-            developer {
-                id.set("supergrecko")
-                name.set("Mats Larsen")
-                email.set("me@supergrecko.com")
-            }
-        }
-
-        scm {
-            connection.set("scm:git:ssh://github.com/vexelabs/bitbuilder.git")
-            developerConnection.set("scm:git:ssh://git@github.com:vexelabs/bitbuilder.git")
-            url.set("https://github.com/vexelabs/bitbuilder")
+            sign(publishing.publications["sonatype"])
         }
     }
-}
-
-signing {
-    if (isCI) {
-        val signingKey: String? by project
-        val signingPassword: String? by project
-
-        useInMemoryPgpKeys(signingKey, signingPassword)
-    } else {
-        useGpgCmd()
-    }
-    sign(publishing.publications["sonatype"])
 }
