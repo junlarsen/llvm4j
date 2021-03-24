daddbf7803c67bba03cc9c8391b663e8cd50facd
Mats Larsen <me@supergrecko.com>
2021-03-24 17:24:11 +0100

    [feat] add target initialization

--- src/main/kotlin/org/llvm4j/llvm4j/LLVMSystem.kt
+++ src/main/kotlin/org/llvm4j/llvm4j/LLVMSystem.kt
@@ -2,9 +2,53 @@ package org.llvm4j.llvm4j
 
 import org.bytedeco.javacpp.BytePointer
 import org.bytedeco.llvm.LLVM.LLVMFatalErrorHandler
+import org.bytedeco.llvm.global.LLVM
 import org.llvm4j.llvm4j.util.Callback
 
 public object LLVMSystem {
+    /** Installs a LLVM fatal error handler */
+    public fun installHandler(handler: FatalErrorHandler) {
+        LLVM.LLVMInstallFatalErrorHandler(handler)
+    }
+
+    /** Reset the fatal error handler */
+    public fun resetHandler() {
+        LLVM.LLVMResetFatalErrorHandler()
+    }
+
+    /**
+     * Enables LLVMs built in stack trace code which intercepts the OS crash signal to print which LLVM component at
+     * the time of the crash
+     */
+    public fun enableStackTrace() {
+        LLVM.LLVMEnablePrettyStackTrace()
+    }
+
+    /**
+     * Initializes the components for a target
+     *
+     * Implementation left out because of its size
+     */
+    public fun initializeTarget(target: TargetArchitecture): Unit = initialize(target)
+
+    /** Initializes all the targets in the LLVM system */
+    public fun initializeAllTargets() {
+        LLVM.LLVMInitializeAllTargets()
+        LLVM.LLVMInitializeAllAsmParsers()
+        LLVM.LLVMInitializeAllTargetInfos()
+        LLVM.LLVMInitializeAllAsmPrinters()
+        LLVM.LLVMInitializeAllAsmParsers()
+        LLVM.LLVMInitializeAllTargetMCs()
+    }
+
+    /** Initializes the target this system is running on */
+    public fun initializeNativeTarget() {
+        LLVM.LLVMInitializeNativeTarget()
+        LLVM.LLVMInitializeNativeDisassembler()
+        LLVM.LLVMInitializeNativeAsmParser()
+        LLVM.LLVMInitializeNativeAsmPrinter()
+    }
+
     public class FatalErrorHandler(private val closure: (Payload) -> Unit) :
         LLVMFatalErrorHandler(),
         Callback<Unit, FatalErrorHandler.Payload> {
@@ -20,5 +64,143 @@ public object LLVMSystem {
         }
 
         public data class Payload(public val details: String)
+    }
+}
+
+private fun initialize(target: TargetArchitecture): Unit = when (target) {
+    TargetArchitecture.AArch64 -> {
+        LLVM.LLVMInitializeAArch64TargetInfo()
+        LLVM.LLVMInitializeAArch64Target()
+        LLVM.LLVMInitializeAArch64TargetMC()
+        LLVM.LLVMInitializeAArch64AsmPrinter()
+        LLVM.LLVMInitializeAArch64AsmParser()
+        LLVM.LLVMInitializeAArch64Disassembler()
+    }
+    TargetArchitecture.AMDGPU -> {
+        LLVM.LLVMInitializeAMDGPUTargetInfo()
+        LLVM.LLVMInitializeAMDGPUTarget()
+        LLVM.LLVMInitializeAMDGPUTargetMC()
+        LLVM.LLVMInitializeAMDGPUAsmPrinter()
+        LLVM.LLVMInitializeAMDGPUAsmParser()
+        LLVM.LLVMInitializeAMDGPUDisassembler()
+    }
+    TargetArchitecture.ARM -> {
+        LLVM.LLVMInitializeARMTargetInfo()
+        LLVM.LLVMInitializeARMTarget()
+        LLVM.LLVMInitializeARMTargetMC()
+        LLVM.LLVMInitializeARMAsmPrinter()
+        LLVM.LLVMInitializeARMAsmParser()
+        LLVM.LLVMInitializeARMDisassembler()
+    }
+    TargetArchitecture.AVR -> {
+        LLVM.LLVMInitializeAVRTargetInfo()
+        LLVM.LLVMInitializeAVRTarget()
+        LLVM.LLVMInitializeAVRTargetMC()
+        LLVM.LLVMInitializeAVRAsmPrinter()
+        LLVM.LLVMInitializeAVRAsmParser()
+        LLVM.LLVMInitializeAVRDisassembler()
+    }
+    TargetArchitecture.BPF -> {
+        LLVM.LLVMInitializeBPFTargetInfo()
+        LLVM.LLVMInitializeBPFTarget()
+        LLVM.LLVMInitializeBPFTargetMC()
+        LLVM.LLVMInitializeBPFAsmPrinter()
+        LLVM.LLVMInitializeBPFAsmParser()
+        LLVM.LLVMInitializeBPFDisassembler()
+    }
+    TargetArchitecture.Hexagon -> {
+        LLVM.LLVMInitializeHexagonTargetInfo()
+        LLVM.LLVMInitializeHexagonTarget()
+        LLVM.LLVMInitializeHexagonTargetMC()
+        LLVM.LLVMInitializeHexagonAsmPrinter()
+        LLVM.LLVMInitializeHexagonAsmParser()
+        LLVM.LLVMInitializeHexagonDisassembler()
+    }
+    TargetArchitecture.Lanai -> {
+        LLVM.LLVMInitializeLanaiTargetInfo()
+        LLVM.LLVMInitializeLanaiTarget()
+        LLVM.LLVMInitializeLanaiTargetMC()
+        LLVM.LLVMInitializeLanaiAsmPrinter()
+        LLVM.LLVMInitializeLanaiAsmParser()
+        LLVM.LLVMInitializeLanaiDisassembler()
+    }
+    TargetArchitecture.MSP430 -> {
+        LLVM.LLVMInitializeMSP430TargetInfo()
+        LLVM.LLVMInitializeMSP430Target()
+        LLVM.LLVMInitializeMSP430TargetMC()
+        LLVM.LLVMInitializeMSP430AsmPrinter()
+        LLVM.LLVMInitializeMSP430AsmParser()
+        LLVM.LLVMInitializeMSP430Disassembler()
+    }
+    TargetArchitecture.Mips -> {
+        LLVM.LLVMInitializeMipsTargetInfo()
+        LLVM.LLVMInitializeMipsTarget()
+        LLVM.LLVMInitializeMipsTargetMC()
+        LLVM.LLVMInitializeMipsAsmPrinter()
+        LLVM.LLVMInitializeMipsAsmParser()
+        LLVM.LLVMInitializeMipsDisassembler()
+    }
+    TargetArchitecture.PowerPC -> {
+        LLVM.LLVMInitializePowerPCTargetInfo()
+        LLVM.LLVMInitializePowerPCTarget()
+        LLVM.LLVMInitializePowerPCTargetMC()
+        LLVM.LLVMInitializePowerPCAsmPrinter()
+        LLVM.LLVMInitializePowerPCAsmParser()
+        LLVM.LLVMInitializePowerPCDisassembler()
+    }
+    TargetArchitecture.RISCV -> {
+        LLVM.LLVMInitializeRISCVTargetInfo()
+        LLVM.LLVMInitializeRISCVTarget()
+        LLVM.LLVMInitializeRISCVTargetMC()
+        LLVM.LLVMInitializeRISCVAsmPrinter()
+        LLVM.LLVMInitializeRISCVAsmParser()
+        LLVM.LLVMInitializeRISCVDisassembler()
+    }
+    TargetArchitecture.Sparc -> {
+        LLVM.LLVMInitializeSparcTargetInfo()
+        LLVM.LLVMInitializeSparcTarget()
+        LLVM.LLVMInitializeSparcTargetMC()
+        LLVM.LLVMInitializeSparcAsmPrinter()
+        LLVM.LLVMInitializeSparcAsmParser()
+        LLVM.LLVMInitializeSparcDisassembler()
+    }
+    TargetArchitecture.SystemZ -> {
+        LLVM.LLVMInitializeSystemZTargetInfo()
+        LLVM.LLVMInitializeSystemZTarget()
+        LLVM.LLVMInitializeSystemZTargetMC()
+        LLVM.LLVMInitializeSystemZAsmPrinter()
+        LLVM.LLVMInitializeSystemZAsmParser()
+        LLVM.LLVMInitializeSystemZDisassembler()
+    }
+    TargetArchitecture.WASM -> {
+        LLVM.LLVMInitializeWebAssemblyTargetInfo()
+        LLVM.LLVMInitializeWebAssemblyTarget()
+        LLVM.LLVMInitializeWebAssemblyTargetMC()
+        LLVM.LLVMInitializeWebAssemblyAsmPrinter()
+        LLVM.LLVMInitializeWebAssemblyAsmParser()
+        LLVM.LLVMInitializeWebAssemblyDisassembler()
+    }
+    TargetArchitecture.X86 -> {
+        LLVM.LLVMInitializeX86TargetInfo()
+        LLVM.LLVMInitializeX86Target()
+        LLVM.LLVMInitializeX86TargetMC()
+        LLVM.LLVMInitializeX86AsmPrinter()
+        LLVM.LLVMInitializeX86AsmParser()
+        LLVM.LLVMInitializeX86Disassembler()
+    }
+    TargetArchitecture.XCore -> {
+        // does not have AsmParser
+        LLVM.LLVMInitializeXCoreTargetInfo()
+        LLVM.LLVMInitializeXCoreTarget()
+        LLVM.LLVMInitializeXCoreTargetMC()
+        LLVM.LLVMInitializeXCoreAsmPrinter()
+        LLVM.LLVMInitializeXCoreDisassembler()
+    }
+    TargetArchitecture.NVPTX -> {
+        // does not have AsmParser or Disassembler
+        LLVM.LLVMInitializeNVPTXTargetInfo()
+        LLVM.LLVMInitializeNVPTXTarget()
+        LLVM.LLVMInitializeNVPTXTargetMC()
+        LLVM.LLVMInitializeNVPTXAsmPrinter()
     }
 }
--- src/main/kotlin/org/llvm4j/llvm4j/Target.kt
+++ src/main/kotlin/org/llvm4j/llvm4j/Target.kt
@@ -4,7 +4,36 @@ import org.bytedeco.llvm.LLVM.LLVMTargetDataRef
 import org.bytedeco.llvm.LLVM.LLVMTargetLibraryInfoRef
 import org.bytedeco.llvm.LLVM.LLVMTargetMachineRef
 import org.bytedeco.llvm.LLVM.LLVMTargetRef
+import org.llvm4j.llvm4j.util.Enumeration
 import org.llvm4j.llvm4j.util.Owner
+
+/**
+ * A listing of the different target architectures the LLVM distribution was built with.
+ *
+ * Note that JavaCPP LLVM does currently not support LLVM experimental targets such as ARC or VE.
+ *
+ * @author Mats Larsen
+ */
+public enum class TargetArchitecture(public override val value: Int) : Enumeration.EnumVariant {
+    AArch64(0),
+    AMDGPU(1),
+    ARM(2),
+    AVR(3),
+    BPF(4),
+    Hexagon(5),
+    Lanai(6),
+    MSP430(7),
+    Mips(8),
+    PowerPC(9),
+    RISCV(10),
+    Sparc(11),
+    SystemZ(12),
+    WASM(13),
+    X86(14),
+    XCore(15),
+    NVPTX(16);
+    public companion object : Enumeration<TargetArchitecture>(values())
+}
 
 public class Target public constructor(ptr: LLVMTargetRef) : Owner<LLVMTargetRef> {
     public override val ref: LLVMTargetRef = ptr
